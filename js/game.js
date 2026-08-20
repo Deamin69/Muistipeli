373c75edb8066d0a4b31b48ca60cf9a6fcc98be3
Deamin69 <Deamin_69@windowslive.com>
2026-08-20 21:27:25 +0300

    Korvattu prompt valikolla

--- js/game.js
+++ js/game.js
@@ -1,22 +1,15 @@
 import { createBoard } from './board.js';
 
-let currentCardCount = 10;
-
 document.addEventListener('DOMContentLoaded', () => {
-    const cardCount = parseInt(prompt("Syötä korttien määrä (parillinen luku):"), 10);
-    if (cardCount % 2 !== 0 || isNaN(cardCount)) {
-        alert("Korttien määrän täytyy olla parillinen luku.");
-        return;
-    }
-
-    currentCardCount = cardCount;
-    createBoard(cardCount);
-
-    //Tsekataan aloita alusta nappi
     const restartBtn = document.getElementById('restart-btn');
-    if (restartBtn) {
-        restartBtn.addEventListener('click', () => {
-            createBoard(currentCardCount);
-        });
-    }
+    const cardSelect = document.getElementById('card-select');
+
+    // Nappia painamalla luodaan valikon arvo ja lauta luodaan
+    restartBtn.addEventListener('click', () => {
+        const selectedCount = parseInt(cardSelect.value, 10);
+        createBoard(selectedCount);
+    });
+
+    // peli luodaan automaattisesti oletusarvolla (16 korttia)
+    createBoard(16);
 });
